dc5fd555f8e26d6e38ac50ef2cab1f05250998be
lanceliu <liuyunfei86@gmail.com>
2016-07-07 21:24:10 +0800

    parse list and detail

--- src/main/java/com/yunheng/scrapy/processor/AmacPageProcessor.java
+++ src/main/java/com/yunheng/scrapy/processor/AmacPageProcessor.java
@@ -1,6 +1,10 @@
 package com.yunheng.scrapy.processor;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.yunheng.scrapy.downloader.JSONRequestDownloader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
@@ -9,7 +13,9 @@ import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.JsonPathSelector;
 import us.codecraft.webmagic.utils.HttpConstant;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 中国证券投资基金业协会:私募基金管理人
@@ -18,6 +24,7 @@ import java.util.List;
  * @date 16/7/6
  */
 public class AmacPageProcessor implements PageProcessor {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     private Site site = Site.me().setDomain("gs.amac.org.cn")
             .addHeader("Content-Type", "application/json");
@@ -29,16 +36,46 @@ public class AmacPageProcessor implements PageProcessor {
     public void process(Page page) {
 
         if (page.getUrl().regex(URL_LIST).match()) {
-            JsonPathSelector jsonPathSelector = new JsonPathSelector("$.content[*].url");
 
-            List<String> childDetails = (List<String>) jsonPathSelector.selectList( page.getRawText() );
-            page.addTargetRequests(page.getHtml().xpath("//div[@class=\"articleList\"]").links().regex("").all());
-            page.addTargetRequests(page.getHtml().links().regex(URL_LIST).all());
-            //文章页
+            JsonPathSelector detailSelector = new JsonPathSelector("$.content[*].url");
+//            List<String> childDetails = (List<String>) detailSelector.selectList(  page.getRawText() ).stream().map(l-> {
+//                return String.format(URL_DETAIL, l);
+//            }).collect(Collectors.toCollection(ArrayList<String>::new));
+//            page.addTargetRequests( childDetails );
+//            page.addTargetRequest(childDetails.get(0));
+
+            List<Request> childDetails = page.getJson().jsonPath("$.content[*]").nodes().stream().map( node->{
+                Request template = new Request();
+                JSONObject json = JSON.parseObject( node.get() );
+                template.setUrl(String.format(URL_DETAIL, json.getString("url")));
+                template.putExtra("managerName", json.getString("managerName")); // 私募基金管理人名称
+                template.putExtra("artificialPersonName", json.getString("artificialPersonName")); // 法定代表人/执行事务合伙人
+                template.putExtra("city", json.getString("regAdrAgg"));// 注册地
+                template.putExtra("registerNo", json.getString("registerNo"));// 登记编号
+                template.putExtra("primaryInvestType", json.getString("primaryInvestType")); // 基金主要类别
+                template.putExtra("establishDate", json.getDate("establishDate"));// 成立时间
+                template.putExtra("registerDate", json.getDate("registerDate"));// 登记时间
+                return template;
+            }).collect(Collectors.toCollection(ArrayList<Request>::new));
+
+            page.addTargetRequest(childDetails.get(0));
+//            try {
+//                if ( Boolean.parseBoolean(  page.getJson().jsonPath("$.last").get()  ) ) {
+//                    int pageNum = Integer.parseInt(  page.getJson().jsonPath("$.number").get() ) + 1;
+//                    page.addTargetRequest( getListRequest(pageNum, PAGE_SIZE) );
+//                }
+//            } catch ( Exception e ) {
+//                logger.warn("", e);
+//            }
         } else {
-            page.putField("title", page.getHtml().xpath("//div[@class='articalTitle']/h2"));
-            page.putField("content", page.getHtml().xpath("//div[@id='articlebody']//div[@class='articalContent']"));
-            page.putField("date", page.getHtml().xpath("//div[@id='articlebody']//span[@class='time SG_txtc']").regex("\\((.*)\\)"));
+            page.putField("managerName", page.getRequest().getExtra("managerName"));
+            page.putField("artificialPersonName", page.getRequest().getExtra("artificialPersonName"));
+            page.putField("city", page.getRequest().getExtra("city"));
+            page.putField("registerNo", page.getRequest().getExtra("registerNo"));
+            page.putField("primaryInvestType", page.getRequest().getExtra("primaryInvestType"));
+            page.putField("establishDate", page.getRequest().getExtra("establishDate"));
+            page.putField("registerDate", page.getRequest().getExtra("registerDate"));
+
         }
     }
 
@@ -56,13 +93,6 @@ public class AmacPageProcessor implements PageProcessor {
         Request request = new Request(String.format(URL_START_URL, pageNum, pageSize));
         request.setMethod( HttpConstant.Method.POST );
         request.putExtra("jsonData", "{}");
-        return request;
-    }
-
-    private static Request getDetailRequest( String detailId ) {
-
-        Request request = new Request(String.format(URL_DETAIL, detailId));
-        request.setMethod( HttpConstant.Method.GET );
         return request;
     }
 }
